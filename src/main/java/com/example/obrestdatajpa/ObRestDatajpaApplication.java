66bd844428aab2004dd182f8ba60e6a2a2cb424c
Carla <mcarlaalexia@gmail.com>
2023-01-18 19:13:26 -0300

    arreglo miercoles

--- src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
+++ src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import com.example.obrestdatajpa.repository.BookRepository;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
--- src/main/java/com/example/obrestdatajpa/entities/Book.java
+++ src/main/java/com/example/obrestdatajpa/entities/Book.java
@@ -11,7 +11,7 @@ public class Book {
     //atributos encapsulados --> con private
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String author;
